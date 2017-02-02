97b8569f8e1ec67ecce416f909f77d1b7f7a4e2a
asmirnov <asmirnov@immo.ru>
2017-02-02 15:31:20 +0300

    dev

---
+++ device/Broadlink_sp2/Broadlink_sp2.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once (PROJ_DIR . "/htdocs/console/controllers/AbstractDevice.php");
+require_once (__DIR__ . "/../../src/include/broadlink.class.php");
+
+class Broadlink_sp2 extends AbstractDevice
+{
+	
+	public function discovery()
+	{
+		//возвращать mac адреса
+		$ret = ['123'];
+		
+		return $ret;
+	}
+	
+	public function getPortsConf()
+	{
+		return array(
+			'a0' => ['AccessType' => 'RW', 'PortReal' => 'power'],
+		);
+	}
+
+	protected function setPortValTempl($port, $val)
+	{
+		//$this->_addr
+		return true;
+	}
+
+	protected function getPortValTempl($port)
+	{
+		return 0;
+	}
+	
+	public function ping()
+	{
+		return true;
+	}
+
+	public function getVersion() { return "0.0.1"; }
+
+}
